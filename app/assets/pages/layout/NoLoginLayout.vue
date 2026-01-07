779bd72116d64fd3d6d93284e57c28e9ed8e01e8
Sylwester Kierzkowski <skierzko@gmail.com>
2026-01-07 20:42:18 +0100

    Added NoLoginLayout

---
+++ app/assets/pages/layout/NoLoginLayout.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import CookiesInfo from '../../components/CookiesInfo.vue';
+import Footer from './Footer.vue';
+import TopBar from './TopBar.vue';
+import PresentationSection from './PresentationSection.vue';
+</script>
+
+<template>
+    <CookiesInfo>
+        This website uses only essential cookies that are necessary for the proper functioning of the system and the user login process.
+        These cookies are not used for analytical or marketing purposes and do not require user consent.
+    </CookiesInfo>
+
+    <TopBar />
+
+    <PresentationSection>
+        <slot name="presentation_section"></slot>
+    </PresentationSection> 
+
+    <slot></slot>
+
+    <Footer />
+</template>
