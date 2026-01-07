ea35e60b96109365f055f32762ff26d0496b533f
Sylwester Kierzkowski <skierzko@gmail.com>
2026-01-07 18:44:19 +0100

    Separating the footer component

---
+++ app/assets/pages/layout/Footer.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <section class="p-6 py-20 border-t-2 bg-slate-900 text-white">
+        <h2 class="mb-8 text-3xl">Built to Showcase Modern Web Development</h2>
+        <p class="mb-2">This platform was created to demonstrate advanced programming capabilities using Symfony 8, Vue 3, TypeScript Tailwind and Docker.</p>
+        <p class="mb-2">It serves as a practical example of a modern, scalable web application — from backend architecture to a responsive, dynamic frontend.</p>
+        <p class="mb-6">The full source code is publicly available on GitHub, where you can explore the implementation details, review best practices, and download the project for your own use.</p>
+        <p class="mb-2">View the source code on GitHub:  <a href="https://github.com/skierzko/Anonymous-Survey" target="_blank" class="text-blue-600 hover:underline">GitHub Repository</a></p>
+    </section>
+
+    <section>
+        <div class="p-6 py-10 bg-gray-800 text-white text-center">
+            <p>&copy; 2026 Sylwester Kierzkowski.</p>
+            <p>Designed and developed by Sylwester Kierzkowski. This project is open for copying, modification, and use for any purpose.</p>
+        </div>
+    </section>
+</template>
