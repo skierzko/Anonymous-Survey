d5f90cb9e9d97b47fa4078dc817b7850f80df542
Sylwester Kierzkowski <skierzko@gmail.com>
2026-01-07 18:48:22 +0100

    Added login and register routes

--- app/assets/router/index.ts
+++ app/assets/router/index.ts
@@ -1,12 +1,24 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 
 import MainPage from '../pages/MainPage.vue'
+import Login from '../pages/Login.vue'
+import Register from '../pages/Register.vue'
 
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
     name: 'MainPage',
     component: MainPage,
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login,
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register,
   },
 ]
 
