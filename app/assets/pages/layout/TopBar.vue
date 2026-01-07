8f4d9609aad35ee9b89da240e0bd8e18e8945c82
Sylwester Kierzkowski <skierzko@gmail.com>
2026-01-07 18:42:46 +0100

    Separated TopBar to component

---
+++ app/assets/pages/layout/TopBar.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import Btn from '../../components/Btn.vue';
+
+</script>
+
+<template>
+    <section class="p-2 flex items-center">
+        <div class="flex-1 text-xl">Anonymous Survey</div>
+        <div>
+            <RouterLink to="/login">
+                <Btn>Login</Btn>
+            </RouterLink>
+
+            <RouterLink to="/register">
+                <Btn>Register</Btn>
+            </RouterLink>
+        </div>
+    </section>
+</template>
