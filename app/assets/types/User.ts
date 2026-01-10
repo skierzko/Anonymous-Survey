a8d0980eaa8f1eeaae57c5524e485cfec3d84006
Sylwester Kierzkowski <skierzko@gmail.com>
2026-01-10 18:28:01 +0100

    Added User model interface

---
+++ app/assets/types/User.ts
@@ -0,0 +1,10 @@
+export interface User {
+    id: number,
+    email: string,
+    roles: object,
+    is_verified: number,
+    agreed_terms: number,
+    created_at: string,
+    updated_at_at: string,
+    // deleted_at: string,
+}
