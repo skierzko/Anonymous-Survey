4203592840e037e99dfb95e61c40d2a0086ea308
Sylwester Kierzkowski <skierzko@gmail.com>
2026-01-10 18:27:19 +0100

    Changed routes meta flags

--- app/assets/router/index.ts
+++ app/assets/router/index.ts
@@ -3,22 +3,41 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import MainPage from '../pages/MainPage.vue'
 import Login from '../pages/Login.vue'
 import Register from '../pages/Register.vue'
+import UserBoard from '../pages/UserBoard.vue'
+import { authGuard } from './guards/authGuard'
 
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
     name: 'MainPage',
     component: MainPage,
+    meta: {
+      guestAccessible: true,
+    }
   },
   {
     path: '/login',
     name: 'Login',
     component: Login,
+    meta: {
+      guestOnly: true,
+    }
   },
   {
     path: '/register',
     name: 'Register',
     component: Register,
+    meta: {
+      guestOnly: true,
+    }
+  },
+  {
+    path: '/user-board',
+    name: 'UserBoard',
+    component: UserBoard,
+    meta: {
+      requiresAuth: true,
+    }
   },
 ]
 
@@ -26,3 +45,5 @@ export const router = createRouter({
   history: createWebHistory(),
   routes,
 })
+
+router.beforeEach(authGuard)
