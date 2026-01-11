a4ea0eff543eba3a3b81383b04a5bd7dc19047c4
Sylwester Kierzkowski <skierzko@gmail.com>
2026-01-11 12:40:14 +0100

    Added survey-creatoy route and component (empty)

--- app/assets/router/index.ts
+++ app/assets/router/index.ts
@@ -1,49 +1,58 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import { authGuard } from './guards/authGuard'
 
 import MainPage from '../pages/MainPage.vue'
 import Login from '../pages/Login.vue'
 import Register from '../pages/Register.vue'
 import UserBoard from '../pages/UserBoard.vue'
-import { authGuard } from './guards/authGuard'
+import SurveyCreator from '../pages/SurveyCreator.vue'
 
 const routes: RouteRecordRaw[] = [
-  {
-    path: '/',
-    name: 'MainPage',
-    component: MainPage,
-    meta: {
-      guestAccessible: true,
-    }
-  },
-  {
-    path: '/login',
-    name: 'Login',
-    component: Login,
-    meta: {
-      guestOnly: true,
-    }
-  },
-  {
-    path: '/register',
-    name: 'Register',
-    component: Register,
-    meta: {
-      guestOnly: true,
-    }
-  },
-  {
-    path: '/user-board',
-    name: 'UserBoard',
-    component: UserBoard,
-    meta: {
-      requiresAuth: true,
-    }
-  },
+    {
+        path: '/',
+        name: 'MainPage',
+        component: MainPage,
+        meta: {
+            guestAccessible: true,
+        }
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: Login,
+        meta: {
+            guestOnly: true,
+        }
+    },
+    {
+        path: '/register',
+        name: 'Register',
+        component: Register,
+        meta: {
+            guestOnly: true,
+        }
+    },
+    {
+        path: '/user-board',
+        name: 'UserBoard',
+        component: UserBoard,
+        meta: {
+            requiresAuth: true,
+        },
+    },
+    {
+        path: '/survey-creator',
+        name: 'SurveyCreator',
+        component: SurveyCreator,
+        meta: {
+            requiresAuth: true,
+        },
+    },
 ]
 
 export const router = createRouter({
-  history: createWebHistory(),
-  routes,
+    history: createWebHistory(),
+    routes,
 })
 
 router.beforeEach(authGuard)
