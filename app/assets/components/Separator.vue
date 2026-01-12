ea2a82e3811bbd60e15452aff28a9fd2223fa328
Sylwester Kierzkowski <skierzko@gmail.com>
2026-01-12 13:38:00 +0100

    Added Separator component

---
+++ app/assets/components/Separator.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+interface Props {
+    label?: string|null,
+}
+
+withDefaults(defineProps<Props>(), {
+    label: 'Add option here'
+});
+</script>
+
+<template>
+    <div class="py-2 opacity-20 flex justify-center items-center hover:opacity-100 cursor-pointer">
+        <div class="h-[1px] flex-1 border-b"></div>
+        <span class="px-2 text-center uppercase text-xs text-gray-500 bg-gray-100">
+            <slot>{{ label }}</slot>
+        </span>
+        <div class="h-[1px] flex-1 border-b"></div>
+    </div>
+</template>
