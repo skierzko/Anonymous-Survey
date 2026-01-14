d80e8ca09166da10e7fe0b5c2740e8edb5cd1266
Sylwester Kierzkowski <skierzko@gmail.com>
2026-01-14 11:44:09 +0100

    Updated component Separator

--- app/assets/components/Separator.vue
+++ app/assets/components/Separator.vue
@@ -2,15 +2,26 @@
 
 interface Props {
     label?: string|null,
+    fullVisibility?: boolean,
+    clickable?: boolean
 }
 
 withDefaults(defineProps<Props>(), {
-    label: 'Add option here'
+    label: 'Add option here',
+    fullVisibility: false,
+    clickable: true,
 });
 </script>
 
 <template>
-    <div class="py-2 opacity-20 flex justify-center items-center hover:opacity-100 cursor-pointer">
+    <div
+        class="flex py-2 justify-center items-center hover:opacity-100"
+        :class="[
+            clickable && 'cursor-pointer',
+            fullVisibility && 'opacity-100',
+            ! fullVisibility && 'opacity-20',
+        ]"
+    >
         <div class="h-[1px] flex-1 border-b"></div>
         <span class="px-2 text-center uppercase text-xs text-gray-500 bg-gray-100">
             <slot>{{ label }}</slot>
