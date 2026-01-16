d10950aea67779a79aeda24352c95c6f8e467257
Sylwester Kierzkowski <skierzko@gmail.com>
2026-01-16 18:19:30 +0100

    Added OpenQuestion

---
+++ app/assets/pages/surveyOptions/openQuestion/OpenQuestion.vue
@@ -0,0 +1,144 @@
+<script setup lang="ts">
+import { ref, inject, onMounted, onUnmounted } from 'vue'
+import type { SurveyQuestionRegistry, SurveyQuestionHandle } from '../../registers/SurveyQuestionRegistry'; 
+import { SurveyQuestion } from '../../models/SurveyQuestion';
+import { ArrowBigUp, ArrowBigDown, Trash2, Eye, EyeOff, Keyboard } from 'lucide-vue-next';
+import Btn from '../../../components/Btn.vue';
+import Separator from '../../../components/Separator.vue';
+import InputText from '../../../components/InputText.vue';
+import InputNumber from '../../../components/InputNumber.vue';
+import OnOff from '../../../components/OnOff.vue';
+
+const props = defineProps<{
+    question: SurveyQuestion,
+    index: number,
+    isFirst: boolean,
+    isLast: boolean,
+}>()
+
+const emit = defineEmits<{
+    (e: 'moveUp', index: number): void,
+    (e: 'moveDown', index: number): void,
+    (e: 'addQuestion', index: number): void,
+    (e: 'remove', index: number): void,
+}>();
+
+/** Validation */
+
+const errors = ref<{
+    title?: boolean,
+    minOptionsLimit?: boolean,
+    maxOptionsLimit?: boolean,
+}>();
+
+const valid = (): boolean => {
+    errors.value = {
+        title: isTitleError(),
+        minOptionsLimit: isMinOptionsLimitError(),
+        maxOptionsLimit: isMaxOptionsLimitError(),
+    };
+
+    return Object.values(errors.value).every(value => value === false);
+}
+
+const isTitleError = (): boolean => {
+    return props.question.title.trim().length === 0;
+}
+
+const isMinOptionsLimitError = (): boolean => {
+    return props.question.minOptionsLimit < 0 ||
+        props.question.minOptionsLimit > props.question.maxOptionsLimit;
+}
+
+const isMaxOptionsLimitError = (): boolean => {
+    return props.question.maxOptionsLimit < 0 ||
+        props.question.maxOptionsLimit < props.question.minOptionsLimit;
+}
+
+/** Registry to parent */
+
+const surveyQuestionRegistry = inject<SurveyQuestionRegistry>('survey-question-registry')
+if (!surveyQuestionRegistry) {
+  throw new Error('survey-question-registry not provided')
+}
+
+const surveyQuestionHandle: SurveyQuestionHandle = { valid }
+
+onMounted(() => {
+  surveyQuestionRegistry.register(surveyQuestionHandle)
+})
+
+onUnmounted(() => {
+  surveyQuestionRegistry.unregister(surveyQuestionHandle)
+})
+
+/** Other */
+
+const toggleVisibility = () => {
+    props.question.visible = !props.question.visible;
+}
+</script>
+
+<template>
+    <Separator @click="emit('addQuestion', index)" label="Add query here" />
+    <div
+        class="bg-gray-100 rounded-xs p-2"
+        :class="[! question.visible && 'opacity-30']"
+    >
+        <div class="flex gap-1 items-center mb-3">
+            <Keyboard class="mr-2" />
+            <div class="flex-1 text-gray-500 font-bold text-xs">
+                Open response
+            </div>
+            <div>
+                <Btn class="p-1!" :disabled="isFirst" @click="emit('moveUp', index)">
+                    <ArrowBigUp :size="20" :stroke-width="1.3" />
+                </Btn>
+            </div>
+            <div>
+                <Btn class="p-1!" :disabled="isLast" @click="emit('moveDown', index)">
+                    <ArrowBigDown :size="20" :stroke-width="1.3" />
+                </Btn>
+            </div>
+            <div>
+                <Btn class="p-1!" :type="question.visible ? 'default' : 'info'">
+                    <Eye v-if="question.visible" :size="20" :stroke-width="1.3" @click="toggleVisibility" />
+                    <EyeOff v-else :size="20" :stroke-width="1.3" @click="toggleVisibility" />
+                </Btn>
+            </div>
+            <div class="ml-6">
+                <Btn type="danger" class="p-1!" @click="emit('remove', index)">
+                    <Trash2 :size="20" :stroke-width="1.3" />
+                </Btn>
+            </div>
+        </div>
+        <div class="grid gap-2 p-2">
+            <InputText
+                v-model="question.title"
+                class="w-3/4 rounded-xs"
+                placeholder="Enter your question"
+                label="Title:"
+                :error="errors?.title ? 'The field cannot remain empty' : ''"
+            />
+            <div class="flex gap-2">
+                <InputNumber
+                    v-model="question.minOptionsLimit"
+                    label="Min. number of characters"
+                    :min="0"
+                    :max="30"
+                    :error="errors?.minOptionsLimit ? 'The field contains errors' : ''"
+                />
+                <InputNumber
+                    v-model="question.maxOptionsLimit"
+                    label="Max. number of characters"
+                    :min="0"
+                    :max="30"
+                    :error="errors?.maxOptionsLimit ? 'The field contains errors' : ''"
+                />
+            </div>
+            <OnOff v-model="question.draft" label="Mark as draft" />
+            <OnOff v-model="question.required" label="Response is required" />
+        </div>
+    </div>
+    <Separator v-if="isLast" label="Add query here" @click="emit('addQuestion', index + 1)" />
+</template>
