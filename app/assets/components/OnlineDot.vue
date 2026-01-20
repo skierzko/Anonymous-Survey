7f4e61f6797b3c9398c53d5d369806a666ea042c
Sylwester Kierzkowski <skierzko@gmail.com>
2026-01-20 23:04:05 +0100

    Added OnlineDot component

---
+++ app/assets/components/OnlineDot.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+defineProps<{
+    offline: boolean,
+}>();
+</script>
+
+<template>
+    <div
+        class="w-[14px] h-[14px] inline-block flex-none rounded-full  p-1"
+        :class="[
+            offline && 'bg-gray-400/30',
+            ! offline && 'bg-emerald-500/30',
+        ]"
+    >
+        <div
+            class="size-1.5 rounded-full "
+            :class="[
+                offline && 'bg-gray-400',
+                ! offline && 'bg-emerald-500',
+            ]"
+        ></div>
+    </div>
+</template>
