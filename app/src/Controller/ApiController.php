b9b8995905035b4df3157a2a09432b385bf68dff
Sylwester Kierzkowski <skierzko@gmail.com>
2026-01-20 23:13:17 +0100

    Updated api controller; Changed me to user; Added surveys; Added surveys/id;

--- app/src/Controller/ApiController.php
+++ app/src/Controller/ApiController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
 use Symfony\Component\Routing\Attribute\Route;
-
 use App\Entity\User;
+use App\Repository\SurveyRepository;
 
 #[Route('/api', name: 'api_')]
 class ApiController extends AbstractController
 {
-    #[Route('/me', name: 'me', methods: ['GET'])]
+    #[Route('/user', name: 'user', methods: ['GET'])]
     public function me(#[CurrentUser] ?User $user): JsonResponse
     {
         if (!$user) {
@@ -25,5 +25,36 @@ class ApiController extends AbstractController
             'id' => $user->getId(),
             'email' => $user->getEmail(),
         ]);
+    }
+
+    #[Route('/surveys', name: 'surveys', methods: ['GET'])]
+    public function surveysByUser(
+        #[CurrentUser] ?User $user,
+        SurveyRepository $surveyRepository,
+    ): JsonResponse {
+        $surveys = $surveyRepository->findBy(['userId' => $user->getId()]);
+
+        return $this->json(
+            $surveys,
+            status: 200,
+            headers: [],
+            context: ['groups' => ['user:read']]
+        );
+    }
+
+    #[Route('/survey/{id}', name: 'survey', methods: ['GET'])]
+    public function surveysByUserAndId(
+        #[CurrentUser] ?User $user,
+        SurveyRepository $surveyRepository,
+        int $id,
+    ): JsonResponse {
+        $survey = $surveyRepository->findBy(['userId' => $user->getId(), 'id' => $id]);
+
+        return $this->json(
+            $survey,
+            status: 200,
+            headers: [],
+            context: ['groups' => ['user:read']]
+        );
     }
 }
