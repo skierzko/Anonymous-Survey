5d06aea55dcd52a6300230b9b27023283b3905ad
Sylwester Kierzkowski <skierzko@gmail.com>
2026-02-03 16:35:09 +0100

    Move survey action to Api controller; Remove SurveyController;

--- app/src/Controller/ApiController.php
+++ app/src/Controller/ApiController.php
@@ -6,8 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
+use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\User;
 use App\Repository\SurveyRepository;
+use App\Dto\Survey\CreateSurveyRequest;
+use App\Service\SurveyService;
 
 #[Route('/api', name: 'api_')]
 class ApiController extends AbstractController
@@ -48,7 +52,11 @@ class ApiController extends AbstractController
         SurveyRepository $surveyRepository,
         int $id,
     ): JsonResponse {
-        $survey = $surveyRepository->findBy(['userId' => $user->getId(), 'id' => $id]);
+        $survey = $surveyRepository->findBy([
+            'userId' => $user->getId(),
+            'id' => $id,
+            'deletedAt' => null
+            ]);
 
         return $this->json(
             $survey,
@@ -56,5 +64,65 @@ class ApiController extends AbstractController
             headers: [],
             context: ['groups' => ['user:read']]
         );
+    }
+
+    #[Route('/survey/save', name: 'save', methods: ['POST'])]
+    public function save(
+        #[CurrentUser]
+        ?User $user,
+        #[MapRequestPayload]
+        CreateSurveyRequest $request,
+        SurveyService $surveyService,
+        EntityManagerInterface $em
+    ): JsonResponse {
+        if (!$user) {
+            return $this->json([
+                'error' => 'User is not logged.'
+            ], 401);
+        }
+
+        $survey = $surveyService->saveSurvey($request, $em);
+
+        return $this->json(
+            $survey,
+            status: 200,
+            headers: [],
+            context: ['groups' => ['user:read']]
+        );
+    }
+
+    #[Route('/survey/{id}', name: 'delete', methods: ['DELETE'])]
+    public function deleteSurvey(
+        #[CurrentUser]
+        ?User $user,
+        int $id,
+        SurveyRepository $surveyRepository,
+        EntityManagerInterface $em
+    ): JsonResponse {
+        if (!$user) {
+            return $this->json([
+                'error' => 'User is not logged.'
+            ], 401);
+        }
+
+        $survey = $surveyRepository->findOneBy([
+            'id' => $id,
+            'userId' => $user->getId(),
+            'deletedAt' => null
+        ]);
+
+        if (!$survey) {
+            return $this->json([
+                'error' => 'Survey not found.'
+            ], 404);
+        }
+
+        $survey->softDelete();
+        $em->persist($survey);
+        $em->flush();
+
+        return $this->json([
+            'success' => true
+        ]);
     }
 }
