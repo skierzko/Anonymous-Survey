496d496ad9df978aa329c0dfcbf7f95b4a9d2fc1
Sylwester Kierzkowski <skierzko@gmail.com>
2026-02-07 13:52:10 +0100

    Added CheckboxQuestionShow component

---
+++ app/assets/pages/surveyOptions/checkboxQuestion/CheckboxQuestionShow.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { SurveyQuestion } from '../../models/SurveyQuestion';
+import Pill from '../../../components/Pill.vue';
+
+const currentOptions = new Map<number, string>();
+
+const props = defineProps<{
+    question: SurveyQuestion,
+}>()
+</script>
+
+<template>
+    <div
+        class="bg-gray-100 rounded-xs p-2"
+        :class="[! question.visible && 'opacity-30']"
+    >
+        <Pill v-if="question.required" variant="danger" size="xs">
+            Required
+            {{ question.minOptionsLimit > 0 ? ` (min ${question.minOptionsLimit})` : '' }}
+            {{ question.maxOptionsLimit > 0 ? ` (max ${question.maxOptionsLimit})` : '' }}
+        </Pill>
+        <div class="grid gap-2 p-2 select-none">
+            {{ question.title }}
+            <div v-for="(option, index) in question.options" :key="option.id" class="ml-6">
+                <input
+                    type="checkbox"
+                    class="cursor-pointer"
+                    :id="option.uuid"
+                    :name="question.uuid"
+                    :value="option.uuid"
+                    v-model="currentOptions"
+                    /> <label :for="option.uuid" class="ml-2 cursor-pointer" >{{ option.title }}</label>
+            </div>
+        </div>
+    </div>
+</template>
