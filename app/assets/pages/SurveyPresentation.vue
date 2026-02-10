132973db3af150c51287753dd87464dad537e8ed
Sylwester Kierzkowski <skierzko@gmail.com>
2026-02-10 19:12:44 +0100

    Added messages to start and end survey presentation; Centered send button;

--- app/assets/pages/SurveyPresentation.vue
+++ app/assets/pages/SurveyPresentation.vue
@@ -8,6 +8,7 @@ import type { SurveyQuestionShowHandle, SurveyQuestionShowRegistry } from './reg
 import Btn from '../components/Btn.vue';
 import { toast } from 'vue3-toastify';
 import { api } from './services/api';
+import { MessageSquareShare } from 'lucide-vue-next';
 
 const surveyStore = useSurveyStore();
 const isReadyToSave = ref<boolean>(false);
@@ -117,6 +118,21 @@ const validateAllData = (): boolean => {
 
                 <template v-else>
                     <div class="grid gap-4 grid-cols-1">
+                        <div>
+                            <NotifyBar
+                                v-if="! isReadyToSave"
+                                variant="default"
+                                class="block! text-center"
+                            >
+                                <p class="p-2">
+                                    You are invited to participate in a survey aimed at collecting opinions and experiences
+                                    necessary for further analysis and the improvement of ongoing activities. Participation
+                                    in the survey is voluntary, and completing it will take only a few minutes. Please read
+                                    the questions carefully and provide responses based on your own experiences.
+                                </p>
+                            </NotifyBar>
+                        </div>
+
                         <template v-for="(surveyQuestion, index) in surveyStore.surveys[0]?.questions" :key="'query-' + index">
                             <component
                                 v-if="SURVEY_COMPONENT_MAP[surveyQuestion.type].s !== null && surveyQuestion.visible && surveyQuestion.draft === false"
@@ -127,11 +143,41 @@ const validateAllData = (): boolean => {
                                 Commponent not found for question type: {{ surveyQuestion.type }}
                             </NotifyBar>
                         </template>
-                        <div class="flex justify-end mt-4">
-                            <Btn variant="primary" @click="send()">
-                                Send survey {{ isReadyToSave }}
+
+                        <div>
+                            <NotifyBar
+                                v-if="! isReadyToSave"
+                                variant="default"
+                                class="block! text-center"
+                            >
+                                <p class="p-2">
+                                    This survey has been designed to collect reliable and valuable information that will help 
+                                    better understand respondents’ opinions and experiences. The results will be used exclusively 
+                                    for analytical and improvement purposes, supporting informed decision-making and 
+                                    the enhancement of ongoing activities.
+                                </p>
+                                <p class="p-2">
+                                    Participation in the survey is <strong>completely anonymous</strong>. This means that none of the responses 
+                                    provided will be linked to any individual or used to identify respondents in any way. 
+                                    The survey includes both required and optional fields — respondents may choose which 
+                                    questions to answer, except for those marked as mandatory.
+                                </p>
+                                <p class="p-2">
+                                    Completing the survey will take only a few minutes. Once all responses have been provided, 
+                                    please submit the form using the <strong>survey submission button located below</strong>. Thank you for your 
+                                    time and contribution — every response is valuable.
+                                </p>
+                            </NotifyBar>
+                        </div>
+                        <div class="flex justify-center mt-4">
+                            <Btn
+                                class="flex! gap-2 px-10! py-4"
+                                variant="primary"
+                                @click="send()"
+                            >
+                                <MessageSquareShare />
+                                Send survey
                             </Btn>
-                            
                         </div>
                     </div>
                 </template>
