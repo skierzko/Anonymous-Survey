5eac83d9b540d80cec51a8811f4e51d584d1c76d
Sylwester Kierzkowski <skierzko@gmail.com>
2026-02-13 18:41:45 +0100

    Added load js and css from manifest

--- app/src/Controller/SpaController.php
+++ app/src/Controller/SpaController.php
@@ -3,19 +3,24 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Utils\ViteAsset;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 
 class SpaController extends AbstractController
 {
-    public function index(CsrfTokenManagerInterface $csrfTokenManager): Response
+    public function index(
+        CsrfTokenManagerInterface $csrfTokenManager,
+        ViteAsset $viteAsset,
+        ): Response
     {
         $token = $csrfTokenManager->getToken('registration')->getValue();
         /** @var User $user */
         $user = $this->getUser();
 
         return $this->render('spa.html.twig', [
+            'vite_asset' => $viteAsset,
             'props' => [
                 'csrfToken' => $token,
                 'userLogged' => $user?->toArray(),
